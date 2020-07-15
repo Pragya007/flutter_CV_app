0cff21e6829b54ee7b8f2356c0816974ffe6ea29
Pragya007 <pragsprincess24@gmail.com>
2020-07-15 11:04:03 +0530

    Refactored code

--- lib/main.dart
+++ lib/main.dart
@@ -146,102 +146,8 @@ class _MyHomePageState extends State<MyHomePage> {
                   ],
                 ),
               ),
-              Container(
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      margin: new EdgeInsets.all(20.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text('Current Pojects',
-                              textAlign: TextAlign.left,
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 20)),
-                          Text('3',
-                              textAlign: TextAlign.right,
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 20)),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      child: SizedBox(
-                        height: 200, // card height
-                        child: PageView.builder(
-                          itemCount: 3,
-                          controller: PageController(viewportFraction: 0.7),
-                          onPageChanged: (int index) =>
-                              setState(() => _index1 = index),
-                          itemBuilder: (_, i) {
-                            return Transform.scale(
-                              scale: i == _index1 ? 1 : 0.9,
-                              child: Card(
-                                elevation: 6,
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20)),
-                                child: Center(
-                                  child: getProjects(_index1),
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-                      ),
-                    ),
-
-                    //card scroll
-                  ],
-                ),
-              ),
-              Container(
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      margin: new EdgeInsets.all(20.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text('Past Experience',
-                              textAlign: TextAlign.left,
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 20)),
-                          Text('3',
-                              textAlign: TextAlign.right,
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 20)),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      child: SizedBox(
-                        height: 100, // card height
-                        child: PageView.builder(
-                          itemCount: 3,
-                          controller: PageController(viewportFraction: 0.8),
-                          onPageChanged: (int index) =>
-                              setState(() => _index = index),
-                          itemBuilder: (_, i) {
-                            return Transform.scale(
-                              scale: i == _index ? 1 : 0.9,
-                              child: Card(
-                                elevation: 6,
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20)),
-                                child: Center(
-                                  child: getPast(_index),
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-                      ),
-                    ),
-
-                    //card scroll
-                  ],
-                ),
-              ),
+              buildContainer('Current Pojects', '3', getProjects),
+              buildContainer('Past Experience', '3', getPast),
               Container(
                 child: Column(
                   children: <Widget>[
@@ -286,10 +192,6 @@ class _MyHomePageState extends State<MyHomePage> {
                         ],
                       ),
                     ),
-
-                    //card scroll
-
-                    // ),
                   ],
                 ),
               ),
@@ -299,149 +201,112 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+
+  Container buildContainer(String title, String number, dynamic func) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: new EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(title,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(color: Colors.white, fontSize: 20)),
+                Text(number,
+                    textAlign: TextAlign.right,
+                    style: TextStyle(color: Colors.white, fontSize: 20)),
+              ],
+            ),
+          ),
+          Container(
+            child: SizedBox(
+              height: 200, // card height
+              child: PageView.builder(
+                itemCount: 3,
+                controller: PageController(viewportFraction: 0.7),
+                onPageChanged: (int index) => setState(() => _index1 = index),
+                itemBuilder: (_, i) {
+                  return Transform.scale(
+                    scale: i == _index1 ? 1 : 0.9,
+                    child: Card(
+                      elevation: 6,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Center(
+                        child: func(_index1),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+
+          //card scroll
+        ],
+      ),
+    );
+  }
 }
 
 getProjects(int i) {
   if (i == 0)
-    return Column(
-      children: <Widget>[
-        Container(
-          margin: new EdgeInsets.all(10.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              // image: new DecoratedBox(decoration: null),
-              Flexible(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text('COVID-19',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                  Text('Website', style: TextStyle(fontSize: 16)),
-                ],
-              )),
-              Container(
-                width: 80.0,
-                height: 80.0,
-                child: DecoratedBox(
-                    decoration: new BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        image: new DecorationImage(
-                            fit: BoxFit.cover,
-                            image: new NetworkImage(
-                                "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTVcACOCd8vdM55Deo3NL2s-Cdl8NNJDFgItIIHZ8R_cbeaMdmX&usqp=CAU")))),
-              ),
-            ],
-          ),
-        ),
-        Flexible(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Website for easy visualisation of statistics of covid-19 in whole world',
-              style: TextStyle(fontSize: 16),
-              textAlign: TextAlign.center,
-            ),
-          ],
-        )),
-      ],
-    );
+    return project('COVID-19', 'Website',
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTVcACOCd8vdM55Deo3NL2s-Cdl8NNJDFgItIIHZ8R_cbeaMdmX&usqp=CAU");
   if (i == 1)
-    return Column(
-      children: <Widget>[
-        Container(
-          margin: new EdgeInsets.all(10.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              // image: new DecoratedBox(decoration: null),
-
-              Flexible(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text('Lost Person Tracker',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                  Text('Python Application', style: TextStyle(fontSize: 16)),
-                ],
-              )),
-              Container(
-                width: 70.0,
-                height: 70.0,
-                child: DecoratedBox(
-                    decoration: new BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        image: new DecorationImage(
-                            fit: BoxFit.cover,
-                            image: new NetworkImage(
-                                "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTTwpw1_XWNB150j45ab9fzsBMVU8ltzSrBXpWatq1RMmvgrC_a&usqp=CAU")))),
-              ),
-            ],
-          ),
-        ),
-        Flexible(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Text(
-              'An application that can be used to find missing person easily and efficiently. Using KNN algorithm this is designed.',
-              style: TextStyle(fontSize: 16),
-              textAlign: TextAlign.center,
-            ),
-          ],
-        )),
-      ],
-    );
+    return project('Lost Person Tracker', 'Python Application',
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTTwpw1_XWNB150j45ab9fzsBMVU8ltzSrBXpWatq1RMmvgrC_a&usqp=CAU");
   if (i == 2)
-    return Column(
-      children: <Widget>[
-        Container(
-          margin: new EdgeInsets.all(10.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              // image: new DecoratedBox(decoration: null),
-              Flexible(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text('Drowsy Driver Monitoring',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                  Text('Raspberry Pi Hardware', style: TextStyle(fontSize: 16)),
-                ],
-              )),
-              Container(
-                width: 70.0,
-                height: 70.0,
-                child: DecoratedBox(
-                    decoration: new BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        image: new DecorationImage(
-                            fit: BoxFit.cover,
-                            image: new NetworkImage(
-                                "https://webstockreview.net/images/driver-clipart-drowsy-driving.png")))),
-              ),
-            ],
-          ),
-        ),
-        Flexible(
-            child: Column(
+    return project('Drowsy Driver Monitoring', 'Raspberry Pi Hardware',
+        "https://webstockreview.net/images/driver-clipart-drowsy-driving.png");
+}
+
+Column project(String name, String tech, String image) {
+  return Column(
+    children: <Widget>[
+      Container(
+        margin: new EdgeInsets.all(10.0),
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Text(
-              'Hardware that can be easily be fitted in any vehical and can be used to alam the driver if he is sleepy.',
-              style: TextStyle(fontSize: 16),
-              textAlign: TextAlign.center,
+            // image: new DecoratedBox(decoration: null),
+            Flexible(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text(name,
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                Text(tech, style: TextStyle(fontSize: 16)),
+              ],
+            )),
+            Container(
+              width: 70.0,
+              height: 70.0,
+              child: DecoratedBox(
+                  decoration: new BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      image: new DecorationImage(
+                          fit: BoxFit.cover, image: new NetworkImage(image)))),
             ),
           ],
-        )),
-      ],
-    );
+        ),
+      ),
+      Flexible(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Text(
+            'Hardware that can be easily be fitted in any vehical and can be used to alam the driver if he is sleepy.',
+            style: TextStyle(fontSize: 16),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      )),
+    ],
+  );
 }
 
 getPast(int i) {
